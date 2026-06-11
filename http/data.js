ac3133cddaf1a29bcec91fa2355a0a46d9c0c047
Prince gond <princegondrw123@gmail.com>
2026-06-11 15:45:15 +0530

    ...

---
+++ http/data.js
@@ -0,0 +1,12 @@
+export const data =[
+    {
+        id:1,
+        name:"QWE12",
+        price:"122"
+    },
+    {
+        id:2,
+        name:"QWE12333",
+        price:"1000"
+    }
+]
--- http/server.js
+++ http/server.js
@@ -1,10 +1,12 @@
 import http from 'http'
 import fs from 'fs'
 import path from 'path'
-
+import { data } from './data.js'
 const server =  http.createServer((req, res)=>{
     if(req.url=="/"&& req.method =="GET"){
-
+        res.writeHead(404,{"content-type":"application/json"})
+        res.end(
+            JSON.stringify(data))
     }
     else{
         res.writeHead(404,{"content-type":"application/json"})
