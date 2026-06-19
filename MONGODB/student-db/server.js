c6f0cfa51f23b38e2c294bf7f65187d0224a7fe7
Prince gond <princegondrw123@gmail.com>
2026-06-19 14:47:38 +0530

    ././

--- MONGODB/student-db/server.js
+++ MONGODB/student-db/server.js
@@ -29,6 +29,33 @@ const studentdb = async()=>{
 
 }
 const getuser = async()=>{
-    
+    const db = await clientDb();
+    const result = await db.collection("student").find().toArray();
+    console.log("students ", result);
 }
+
+const updatedb = async () =>{
+    const db = await clientDb();
+    const result = await db.collection("student").updateOne(
+        {_id: new ObjectId("6a33ec79da269ffa0f1c4472")}
+        ,{$set:{name:"Prince",
+            course:"Full Stack Web Development"
+        }}
+)
+
+}
+ 
+const findone = async()=>{
+    const db = await clientDb();
+    const result =  await db.collection("student").findOne({name:"Prince"});
+    console.log(result)
+}
+
 // studentdb();
+// getuser();
+// updatedb();
+// findone();// ye mangodb ko bolega ki student naam ki collection dhundo aur usme se prince naame ka oura data laao 
+
+
+
+
